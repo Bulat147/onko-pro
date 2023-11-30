95dd4e3431e24d969fd5c5ad29156fd4dc3dddbe
Aidar <ajdar.gafiat@gmail.com>
2023-11-30 10:35:27 +0300

    deleted account_id column from article table

--- backend/db/src/main/resources/liquibase/changelog/v1/DDL/SQL/20231129-create-article-table.sql
+++ backend/db/src/main/resources/liquibase/changelog/v1/DDL/SQL/20231129-create-article-table.sql
@@ -3,6 +3,5 @@ CREATE TABLE article
     id            uuid PRIMARY KEY             DEFAULT uuid_generate_v4(),
     title varchar(64) NOT NULL,
     content varchar(2048) NOT NULL,
-    account_id uuid NOT NULL REFERENCES account (id),
     position varchar(32) NOT NULL
 )
